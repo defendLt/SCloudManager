7d5bc7716b4fee45dba158b9ea52593c38cb56b1
PlatonovDmitriy <platdm-it@yandex.ru>
2020-09-07 01:51:45 +0300

    add| replace kotlin synthetic with view binding for domain

--- app/src/main/java/com/platdmit/simplecloudmanager/screens/domain/DomainFragment.kt
+++ app/src/main/java/com/platdmit/simplecloudmanager/screens/domain/DomainFragment.kt
@@ -4,31 +4,38 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.platdmit.domain.models.Domain
 import com.platdmit.simplecloudmanager.R
+import com.platdmit.simplecloudmanager.base.extensions.setLoaderStatus
+import com.platdmit.simplecloudmanager.databinding.FragmentDomainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_domain.*
 
 @AndroidEntryPoint
 class DomainFragment : Fragment(R.layout.fragment_domain) {
     private val domainViewModel: DomainViewModel by viewModels()
+    private val domainViewBinding: FragmentDomainBinding by viewBinding()
     private val domainRecordListAdapter = DomainRecordListAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        domain_record_list.layoutManager = LinearLayoutManager(context)
-        domainViewModel.domainStateLiveData.observe(viewLifecycleOwner, Observer { stateHandler(it) })
+        domainViewBinding.domainRecordList.layoutManager = LinearLayoutManager(context)
+        domainViewModel.domainStateLiveData.observe(viewLifecycleOwner, { stateHandler(it) })
     }
 
     private fun stateHandler(domainState: DomainState){
         when(domainState){
-            is DomainState.Loading -> {}
+            is DomainState.Loading -> {
+                setLoaderStatus(true)
+            }
             is DomainState.Success -> {
                 initData(domainState.domain)
+                setLoaderStatus(false)
             }
-            is DomainState.Empty -> {}
+            is DomainState.Empty -> {
+                setLoaderStatus(false)
+            }
             is DomainState.Error -> {}
         }
     }
@@ -38,11 +45,11 @@ class DomainFragment : Fragment(R.layout.fragment_domain) {
     }
 
     private fun initData(domain: Domain) {
-        domain_name.text = domain.name
-        domain_type.text = domain.type
+        domainViewBinding.domainName.text = domain.name
+        domainViewBinding.domainType.text = domain.type
         domain.domainRecords?.let { domainRecordListAdapter.setContentData(it) }
-        if (domain_record_list.adapter == null) {
-            domain_record_list.adapter = domainRecordListAdapter
+        if (domainViewBinding.domainRecordList.adapter == null) {
+            domainViewBinding.domainRecordList.adapter = domainRecordListAdapter
         } else {
             domainRecordListAdapter.notifyDataSetChanged()
         }
