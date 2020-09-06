fb5ee7cc724aef601f22ce3709c5afd81ce06245
PlatonovDmitriy <platdm-it@yandex.ru>
2020-09-07 02:30:52 +0300

    add| replace kotlin synthetic with view binding for server load average tab

--- app/src/main/java/com/platdmit/simplecloudmanager/screens/server/tabloadaverage/ServerTabMainFragment.kt
+++ app/src/main/java/com/platdmit/simplecloudmanager/screens/server/tabloadaverage/ServerTabMainFragment.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.platdmit.domain.models.LoadAverage
 import com.platdmit.simplecloudmanager.R
+import com.platdmit.simplecloudmanager.databinding.FragmentServerTabMainBinding
 import com.platdmit.simplecloudmanager.screens.server.ServerTabFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_server_tab_main.*
 
 @AndroidEntryPoint
 class ServerTabMainFragment(
         private val mTitle: String = "empty"
 ) : Fragment(R.layout.fragment_server_tab_main), ServerTabFragment<ServerTabMainFragment> {
     private val loadAverageViewModel: LoadAverageViewModel by viewModels()
+    private val loadAverageViewBinding: FragmentServerTabMainBinding by viewBinding()
     private val loadAverageListAdapter = LoadAverageListAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        server_load_average.layoutManager = LinearLayoutManager(context)
-        loadAverageViewModel.loadAverageStateLiveData.observe(viewLifecycleOwner, Observer { stateHandler(it) })
+        loadAverageViewBinding.serverLoadAverage.layoutManager = LinearLayoutManager(context)
+        loadAverageViewModel.loadAverageStateLiveData.observe(viewLifecycleOwner, { stateHandler(it) })
     }
 
     override fun getTitle(): String {
@@ -51,8 +52,8 @@ class ServerTabMainFragment(
 
     private fun updateServerLoadAverageData(loadAverages: List<LoadAverage>) {
         loadAverageListAdapter.setContentData(loadAverages)
-        if (server_load_average.adapter == null) {
-            server_load_average.adapter = loadAverageListAdapter
+        if (loadAverageViewBinding.serverLoadAverage.adapter == null) {
+            loadAverageViewBinding.serverLoadAverage.adapter = loadAverageListAdapter
         } else {
             loadAverageListAdapter.notifyDataSetChanged()
         }
