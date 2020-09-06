e69c99522912eddb6ce7f1b90f49680e0bfd1f0c
PlatonovDmitriy <platdm-it@yandex.ru>
2020-09-07 02:15:49 +0300

    add| replace kotlin synthetic with view binding for server

--- app/src/main/java/com/platdmit/simplecloudmanager/screens/server/ServerFragment.kt
+++ app/src/main/java/com/platdmit/simplecloudmanager/screens/server/ServerFragment.kt
@@ -5,27 +5,28 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
 import com.platdmit.domain.models.Server
 import com.platdmit.simplecloudmanager.R
+import com.platdmit.simplecloudmanager.databinding.FragmentServerBinding
 import com.platdmit.simplecloudmanager.screens.server.tabactions.ServerTabActionsFragment
 import com.platdmit.simplecloudmanager.screens.server.tabbackups.ServerTabBackupsFragment
 import com.platdmit.simplecloudmanager.screens.server.tabloadaverage.ServerTabMainFragment
 import com.platdmit.simplecloudmanager.screens.server.tabstatistics.ServerTabStatisticsFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_server.*
 
 @AndroidEntryPoint
 class ServerFragment : Fragment(R.layout.fragment_server) {
     private val serverViewModel: ServerViewModel by viewModels()
+    private val serverViewBinding: FragmentServerBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        serverViewModel.serverStateLiveData.observe(viewLifecycleOwner, Observer { stateHandler(it) })
+        serverViewModel.serverStateLiveData.observe(viewLifecycleOwner, { stateHandler(it) })
 
         val serverViewPagerAdapter = ServerViewStateAdapter
                 .add(ServerTabMainFragment(resources.getString(R.string.server_tab_main)))
@@ -35,8 +36,8 @@ class ServerFragment : Fragment(R.layout.fragment_server) {
                 .build(childFragmentManager, lifecycle, requireArguments().getLong("ELEMENT_ID"))
 
         serverViewPagerAdapter?.let {
-            view_pager.adapter = it
-            TabLayoutMediator(tabs, view_pager, TabConfigurationStrategy {
+            serverViewBinding.viewPager.adapter = it
+            TabLayoutMediator(serverViewBinding.tabs, serverViewBinding.viewPager, TabConfigurationStrategy {
                 tab: TabLayout.Tab, position: Int -> tab.text = it.getPageTitle(position)
             }).attach()
         }
@@ -59,12 +60,14 @@ class ServerFragment : Fragment(R.layout.fragment_server) {
 
     @SuppressLint("SetTextI18n")
     private fun initData(server: Server) {
-        server_image_name.text = server.name
-        server_ip.text = resources.getString(R.string.server_ip_pref) + server.v4Ip
-        server_status.text = server.status
-        server_id.text = resources.getString(R.string.server_id_pref) + server.id
-        server_price.text = server.paymentPrice
-        server_paymentDate.text = server.paymentDate
-        activity?.actionBar?.title = server.name
+        server.run {
+            serverViewBinding.serverImageName.text = name
+            serverViewBinding.serverId.text = resources.getString(R.string.server_ip_pref) + v4Ip
+            serverViewBinding.serverStatus.text = status
+            serverViewBinding.serverId.text = resources.getString(R.string.server_id_pref) + id
+            serverViewBinding.serverPrice.text = paymentPrice
+            serverViewBinding.serverPaymentDate.text = paymentDate
+            activity?.actionBar?.title = name
+        }
     }
 }
